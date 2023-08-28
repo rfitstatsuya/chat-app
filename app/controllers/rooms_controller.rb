bbbafef39f2d0750704466e0b87d086c99797416
rfitstatsuya <142726441+rfitstatsuya@users.noreply.github.com>
2023-08-28 14:14:42 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,4 +4,12 @@ class RoomsController < ApplicationController
     @room = Room.new
   end
 
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
